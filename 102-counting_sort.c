152d1163881203856833ff0dc5906c7f9051db91
ChukaCSTD <chuka.ozoadibe@gmail.com>
2024-04-16 16:12:22 -0700

    sorted tasks

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -9,8 +9,8 @@
  */
 void counting_sort(int *array, size_t size)
 {
-	int *counter, a;
-	size_t i, k, arr_size;
+	int *counter, k;
+	size_t i, j, arr_size;
 
 	if (array == NULL || size <= 1)
 		return;
@@ -38,11 +38,11 @@ void counting_sort(int *array, size_t size)
 
 	print_array(counter, arr_size);
 
-	for (i = 1, k = 0; i <= arr_size; i++)
+	for (i = 1, j = 0; i <= arr_size; i++)
 		if (counter[i] != counter[i - 1])
 		{
-			for (a = 0; a < counter[i] - counter[i - 1]; a++)
-				array[k++] = i;
+			for (k = 0; k < counter[i] - counter[i - 1]; k++)
+				array[j++] = i;
 		}
 	free(counter);
 }
